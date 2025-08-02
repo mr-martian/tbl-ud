d7646ebcf96876a0953d7d65b9ab222ee149eb10
Daniel Swanson <awesomeevildudes@gmail.com>
2025-08-02 10:17:38 -0400

    less-exponential context generation

--- round5.context.py
+++ round5.context.py
@@ -9,6 +9,7 @@ sys.path.insert(0, '/home/daniel/apertium/cg3/python')
 import cg3
 
 import argparse
+from collections import Counter, defaultdict
 from itertools import combinations
 import sqlite3
 
@@ -48,7 +49,7 @@ def format_rule(rtype, target, tags=None, desttags=None, context=None):
     elif rtype == 'append':
         return f'APPEND ({tags}) (*) IF (0 ({target})) {ctx} ;'
     elif rtype == 'addcohort':
-        return f'ADDCOHORT ("<ins>" {tags}) BEFORE (*) IF (0 ({target})) {ctx} ;'
+        return f'ADDCOHORT ("<ins>" {tags} @dep) BEFORE (*) IF (0 ({target})) {ctx} ;'
     elif rtype == 'rem-self':
         return f'REMCOHORT (*) IF (0 ({target})) {ctx} ;'
     elif rtype == 'rem-parent':
@@ -62,70 +63,87 @@ def format_relation(target, context):
     ret.append(f'ADDRELATION (tr{{NUM}}) (*) (0 ({target})) TO (0 (*)) ;')
     for test in context:
         ret.append(f'ADDRELATION (r{{NUM}}) (*) (0 ({target})) TO ({test}) ;')
+    # TODO: NEGATE
     return '\n'.join(ret)
 
 def make_rule(target, context, dct):
     return (format_rule(target=target, context=context, **dct),
             format_relation(target, context))
 
-def describe_cohort(cohort):
-    if cohort.static.tags:
-        yield f'{cohort.static.lemma} {cohort.static.tags[0]}'
-        yield cohort.static.tags[0]
+def get_rel(cohort):
     for r in cohort.readings:
         for t in r.tags:
             if t[0] == '@':
-                yield t
-                yield r.tags[0] + ' ' + t
-        break
+                return t
 
-def gen_contexts(slw, idx, dct, include_parent=False):
-    ctx = []
-    pctx = []
+all_rels = set(get_rel(c) for w in source for c in w.cohorts)
+
+def describe_cohort(cohort):
+    rel = get_rel(cohort) or ''
+    if cohort.static.tags:
+        yield f'{cohort.static.lemma} {cohort.static.tags[0]} {rel}'
+    for rd in cohort.readings:
+        yield f'{rd.tags[0]} {rel}'
+        yield f'{rd.lemma} {rd.tags[0]} {rel}'
+        # TODO: features
+
+def collect_contexts(slw, idx, dct):
     ds = slw.cohorts[idx].dep_self
     dh = slw.cohorts[idx].dep_parent
+    s_rel = set()
+    c_rel = set()
     for cohort in slw.cohorts:
-        rel = None
         if cohort.dep_self == dh:
-            rel = 'p'
+            dct['p'].update(set(describe_cohort(cohort)))
         elif cohort.dep_parent == dh and cohort.dep_self != ds:
-            rel = 's'
+            dct['s'].update(set(describe_cohort(cohort)))
+            s_rel.add(get_rel(cohort))
         elif cohort.dep_parent == ds:
-            rel = 'c'
-        if rel is not None:
-            for desc in describe_cohort(cohort):
-                tst = f'{rel} ({desc})'
-                if include_parent and rel == 'p':
-                    pctx.append(tst)
-                else:
-                    ctx.append(tst)
-    tgt = list(describe_cohort(slw.cohorts[idx]))
-    for t in tgt:
-        if include_parent:
-            for cp in pctx:
-                for i in range(args.ctx):
-                    for seq in combinations(ctx, i):
-                        yield make_rule(t, set(seq+(cp,)), dct)
-        else:
-            for i in range(args.ctx+1):
-                for seq in combinations(ctx, i):
-                    yield make_rule(t, set(seq), dct)
+            dct['c'].update(set(describe_cohort(cohort)))
+            c_rel.add(get_rel(cohort))
+    dct['t'].update(set(describe_cohort(slw.cohorts[idx])))
+    #dct['negs'].update(all_rels - s_rel)
+    #dct['negc'].update(all_rels - c_rel)
+
+def rel_ranges(include_parent):
+    mn = 1 if include_parent else 0
+    for n in range(1, args.ctx+1):
+        for i_p in range(mn, 2):
+            for i_s in range(n-i_p+1):
+                yield i_p, i_s, (n-i_p-i_s)
+
+def gen_rules(contexts, dct, include_parent=False):
+    ct = args.count >> 2
+    cp = [(f'p ({k})',v) for k,v in contexts['p'].most_common(ct)]
+    cs = [(f's ({k})',v) for k,v in contexts['s'].most_common(ct)]
+    cs += [(f'NEGATE s ({k})',v) for k,v in contexts['negs'].most_common(ct)]
+    cc = [(f'c ({k})',v) for k,v in contexts['c'].most_common(ct)]
+    cc += [(f'NEGATE c ({k})',v) for k,v in contexts['negc'].most_common(ct)]
+    for i_p, i_s, i_c in rel_ranges(include_parent):
+        for t_p in combinations(cp, i_p):
+            for t_s in combinations(cs, i_s):
+                for t_c in combinations(cc, i_c):
+                    seq = t_p + t_s + t_c
+                    ctx = tuple([s[0] for s in seq])
+                    count = min(s[1] for s in seq)
+                    for tgc, tgi in contexts['t'].most_common(ct):
+                        yield make_rule(tgc, ctx, dct) + (min(count, tgi),)
 
 cur.execute('SELECT COUNT(*) AS ct, rule, tags1, tags2, cohort_key FROM errors GROUP BY rule, tags1, tags2, cohort_key ORDER BY ct DESC LIMIT ?', (args.count,))
 
 patterns = cur.fetchall()
 for count, rule, tags1, tags2, ckey in patterns:
-    rules = []
     cur.execute('SELECT window, cohort FROM errors WHERE rule = ? AND tags1 = ? AND tags2 = ? AND cohort_key = ?',
                 (rule, tags1, tags2, ckey))
+    dct = defaultdict(Counter)
     for wnum, cnum in cur.fetchall():
         slw = source[wnum]
-        for r in gen_contexts(
-                slw, cnum,
-                {'rtype': rule, 'tags': tags1, 'desttags': tags2},
-                include_parent=(rule == 'rem-parent')):
-            rules.append(r)
-    cur.executemany('INSERT INTO context VALUES(?, ?, 1) ON CONFLICT(rule) DO UPDATE SET count = count + 1', rules)
+        collect_contexts(slw, cnum, dct)
+    rules = list(gen_rules(
+        dct,
+        {'rtype': rule, 'tags': tags1, 'desttags': tags2},
+        include_parent=(rule == 'rem-parent')))
+    cur.executemany('INSERT INTO context VALUES(?, ?, ?) ON CONFLICT(rule) DO UPDATE SET count = count + excluded.count', rules)
     con.commit()
 
 if PROFILE:
