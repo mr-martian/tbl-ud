843ed567d939079e3ae57b23552d47e0245af60a
Daniel Swanson <awesomeevildudes@gmail.com>
2025-10-30 16:51:54 -0400

    more powerful context tests

--- round11.py
+++ round11.py
@@ -1,3 +1,10 @@
+PROFILING = False
+PROFILER = None
+if PROFILING:
+    import cProfile
+    PROFILER = cProfile.Profile()
+    PROFILER.enable()
+
 from cg3 import parse_binary_stream as parse_cg3
 
 import argparse
@@ -20,6 +27,7 @@ PROTECT (SOURCE) ;
 
 
 '''
+LEAF_POS = ['CCONJ', 'ADP', 'DET', 'PUNCT', 'INTJ', 'PART', 'AUX']
 
 parser = argparse.ArgumentParser()
 parser.add_argument('source')
@@ -35,6 +43,8 @@ parser.add_argument('--beam', type=int, default=25,
                     help='max instantiations of a single pattern')
 parser.add_argument('--rule_count', type=int, default=25,
                     help='number of rules to try')
+parser.add_argument('--context_similarity', type=float, default=0.9,
+                    help='threshold for discarding more complex context as equivalent')
 args = parser.parse_args()
 
 WEIGHTS = defaultdict(lambda: 1, json.loads(args.weights))
@@ -52,6 +62,12 @@ def desc_c(cohort):
         if 'SOURCE' in r.tags:
             return desc_r(r)
     return desc_r(cohort.readings[0])
+
+def get_rel(cohort):
+    for r in cohort.readings:
+        for t in r.tags:
+            if t[0] == '@':
+                return t
 
 def tags_to_feature_dict(tags, dct=None):
     if dct is None:
@@ -79,7 +95,6 @@ with open(args.target, 'rb') as fin:
 target_words_and_feats = [collect_words_and_feats(w) for w in target]
 
 def gen_rules(window, slw, tlw):
-    LEAF_POS = ['CCONJ', 'ADP', 'DET', 'PUNCT', 'INTJ', 'PART', 'AUX']
     rules = []
     src_words, src_feats = collect_words_and_feats(slw)
     tgt_words, tgt_feats = target_words_and_feats[window]
@@ -87,6 +102,7 @@ def gen_rules(window, slw, tlw):
     missing = +(tgt_words - src_words)
     for idx, cohort in enumerate(slw.cohorts):
         suf = (window, idx, desc_c(cohort))
+        rel = get_rel(cohort)
         inserting = not any(l in suf[2] for l in LEAF_POS)
         words = [desc_r(r) for r in cohort.readings if 'SOURCE' not in r.tags]
         if all(w in extra for w in words):
@@ -102,11 +118,11 @@ def gen_rules(window, slw, tlw):
                     if 'PUNCT' in m:
                         if not (cohort.static.lemma == '<ins>' in suf[2] or 'PUNCT' in suf[2]):
                             continue
-                    rules.append(('append', m, '')+suf)
+                    rules.append(('append', m+' '+rel, '')+suf)
             else:
                 for m in missing:
                     if any(l in m and l in suf[2] for l in LEAF_POS):
-                        rules.append(('append', m, '')+suf)
+                        rules.append(('append', m+' '+rel, '')+suf)
         if len(words) > 1:
             for w in words:
                 if w in extra:
@@ -130,7 +146,7 @@ def gen_rules(window, slw, tlw):
                     continue
                 st = sf[k].total()
                 tt = tf[k].total()
-                if st < tt:
+                if st > tt:
                     rules.append(('substitute', f'{k}={v}', '*')+suf)
                 for v2 in tf[k]:
                     if v != v2 and sf[k][v2] < tf[k][v2]:
@@ -154,42 +170,57 @@ def format_rule(rtype, target, tags=None, desttags=None, context=None):
         ls = sorted(context or [])
     ctx = ' '.join(f'({test})' for test in ls)
     if rtype == 'remove':
-        return f'REMOVE ({tags}) IF (0 ({target})) {ctx} ;'
+        return f'REMOVE ({tags}) IF (0 {target}) {ctx} ;'
     elif rtype == 'append':
-        return f'APPEND ({tags}) (*) IF (0 ({target})) {ctx} ;'
+        return f'APPEND ({tags}) (*) IF (0 {target}) {ctx} ;'
     elif rtype == 'addcohort':
-        return f'ADDCOHORT ("<ins>" {tags} @dep) BEFORE (*) IF (0 ({target})) (NEGATE c ({tags})) {ctx} ;'
+        return f'ADDCOHORT ("<ins>" {tags} @dep) BEFORE (*) IF (0 {target}) (NEGATE c ({tags})) {ctx} ;'
     elif rtype == 'rem-self':
-        return f'REMCOHORT (*) IF (0 ({target})) (NEGATE c (*)) {ctx} ;'
+        return f'REMCOHORT (*) IF (0 {target}) (NEGATE c (*)) {ctx} ;'
     elif rtype == 'rem-parent':
-        return f'WITH (*) IF (0 ({target})) {ctx} {{\n\tSWITCHPARENT WITHCHILD (*) (*) ;\n\tREMCOHORT _C2_ ;\n}} ;'
+        return f'WITH (*) IF (0 {target}) {ctx} {{\n\tSWITCHPARENT WITHCHILD (*) (*) ;\n\tREMCOHORT _C2_ ;\n}} ;'
 
     elif rtype == 'substitute':
-        return f'SUBSTITUTE ({tags}) ({desttags}) (*) IF (0 ({target})) {ctx} ;'
+        return f'SUBSTITUTE ({tags}) ({desttags}) (*) IF (0 {target}) {ctx} ;'
 
 def format_relation(target, context):
     ret = []
-    ret.append(f'ADDRELATION (tr{{NUM}}) (*) (0 ({target})) TO (0 (*)) ;')
+    ret.append(f'ADDRELATION (tr{{NUM}}) (*) (0 {target}) TO (0 (*)) ;')
     for test in context:
-        ret.append(f'ADDRELATION (r{{NUM}}) (*) (0 ({target})) TO ({test}) ;')
+        ret.append(f'ADDRELATION (r{{NUM}}) (*) (0 {target}) TO (A{test}) ;')
     # TODO: NEGATE
     return '\n'.join(ret)
 
-def get_rel(cohort):
-    for r in cohort.readings:
-        for t in r.tags:
-            if t[0] == '@':
-                return t
-
-def describe_cohort(cohort):
+def describe_cohort(cohort, window):
     rel = get_rel(cohort) or ''
+    prel = ''
+    cpos = set()
+    for c in window.cohorts:
+        if c.dep_self == cohort.dep_parent:
+            prel = get_rel(c)
+        elif c.dep_parent == cohort.dep_self:
+            for r in c.readings:
+                t = r.tags[0]
+                if t == 'SOURCE':
+                    t = r.tags[1]
+                if t in LEAF_POS:
+                    cpos.add(t)
     for rd in cohort.readings:
         tag = rd.tags[0]
         if tag == 'SOURCE':
             tag += ' ' + rd.tags[1]
-        yield f'{tag} {rel}'
-        yield f'{rd.lemma} {tag} {rel}'
-        # TODO: features
+        yield f'({tag} {rel})'
+        yield f'({rd.lemma} {tag} {rel})'
+        for feat in rd.tags:
+            if '=' in feat:
+                yield f'({tag} {rel} {feat})'
+                yield f'({rd.lemma} {tag} {rel} {feat})'
+        for cp in cpos:
+            yield f'({tag} {rel}) LINK c ({cp})'
+            yield f'({rd.lemma} {tag} {rel}) LINK c ({cp})'
+        if prel:
+            yield f'({tag} {rel}) LINK p ({prel})'
+            yield f'({rd.lemma} {tag} {rel}) LINK p ({prel})'
 
 def collect_neighbors(slw, idx, dct):
     ds = slw.cohorts[idx].dep_self
@@ -205,8 +236,7 @@ def collect_neighbors(slw, idx, dct):
             dct['c'].add(cohort.dep_self)
 
 def select_contexts(cur, dct):
-    RANGE = 5
-    SIMILARITY = 0.9
+    RANGE = 10
     ret = defaultdict(Counter)
     for rel in dct:
         cs = list(dct[rel])
@@ -216,15 +246,25 @@ def select_contexts(cur, dct):
             cs)
         d2 = defaultdict(set)
         c2 = Counter()
-        for c, p in cur.fetchall():
+        # shorter tests first
+        for c, p in sorted(cur.fetchall(), key=lambda r: len(r[1])):
+            if rel == 't' and 'LINK' in p:
+                continue
             d2[p].add(c)
             c2[p] += 1
         ls = c2.most_common()
         for i in range(len(ls)):
             pattern, count = ls[i]
+            # stop when we get to comparatively low frequency tests
+            if count < ls[0][1] / 10:
+                break
+            # only consider the last RANGE options, rather than all
+            # => most similarly sized
             for j in range(max(i-RANGE, 0), i):
                 pj = ls[j][0]
-                if len(d2[pattern].intersection(d2[pj])) / count >= SIMILARITY:
+                comp = len(d2[pattern].intersection(d2[pj])) / count
+                #print(pattern, count, pj, ls[j][1], comp)
+                if comp >= args.context_similarity:
                     break
             else:
                 ret[rel][pattern] = count
@@ -239,11 +279,11 @@ def rel_ranges(include_parent):
 
 def contextualize_rules(contexts, dct, ekey, include_parent=False):
     ct = args.count >> 2
-    cp = [(f'p ({k})',v) for k,v in contexts['p'].most_common(ct)]
-    cs = [(f's ({k})',v) for k,v in contexts['s'].most_common(ct)]
-    cs += [(f'NEGATE s ({k})',v) for k,v in contexts['negs'].most_common(ct)]
-    cc = [(f'c ({k})',v) for k,v in contexts['c'].most_common(ct)]
-    cc += [(f'NEGATE c ({k})',v) for k,v in contexts['negc'].most_common(ct)]
+    cp = [(f'p {k}',v) for k,v in contexts['p'].most_common(ct)]
+    cs = [(f's {k}',v) for k,v in contexts['s'].most_common(ct)]
+    cs += [(f'NEGATE s {k}',v) for k,v in contexts['negs'].most_common(ct)]
+    cc = [(f'c {k}',v) for k,v in contexts['c'].most_common(ct)]
+    cc += [(f'NEGATE c {k}',v) for k,v in contexts['negc'].most_common(ct)]
     for i_p, i_s, i_c in rel_ranges(include_parent):
         for t_p in combinations(cp, i_p):
             for t_s in combinations(cs, i_s):
@@ -352,7 +392,7 @@ with (TemporaryDirectory() as tmpdir,
             for ch in slw.cohorts:
                 cur.executemany(
                     'INSERT INTO tests(cohort, pattern) VALUES(?, ?)',
-                    [(ch.dep_self, dc) for dc in describe_cohort(ch)])
+                    [(ch.dep_self, dc) for dc in describe_cohort(ch, slw)])
             con.commit()
 
         patterns = []
@@ -427,3 +467,7 @@ with (TemporaryDirectory() as tmpdir,
         subprocess.run(['vislcg3', '--in-binary', '--out-binary', '-g',
                         gpath, '-I', src_path, '-O', tgt_path],
                        capture_output=True)
+
+if PROFILING:
+    PROFILER.disable()
+    PROFILER.dump_stats('r11.stats')
