fbd2aad8dc5ed738c85f2cb0c62aa49d38e3b60d
Daniel Swanson <awesomeevildudes@gmail.com>
2025-12-11 16:20:43 -0500

    generate hbo-grc data

--- hbo-grc.sh
+++ hbo-grc.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-python3 dix_by_gloss.py generated/hbo-grc/hbo.conllu generated/hbo-grc/grc.conllu -t NOUN:Gender -s NOUN:Gender -s VERB:HebBinyan -s AUX:HebBinyan -s DET:PronType -t DET:PronType -s PROPN:Gender -t PROPN:Gender generated/hbo-grc/gloss.dix
+hin=~/hbo-UD/UD_Ancient_Hebrew-PTNK/hbo_ptnk-ud-train.conllu
+gin=~/UD_Ancient_Greek-PTNK/grc_ptnk-ud-train.conllu
+
+make_grc() {
+  python3 conllu2apertium.py --count "$1" --surface NOUN:Gender DET:PronType PROPN:Gender | cg-conv -a -Z --dep-delimit
+}
+
+make_hbo() {
+  python3 conllu2apertium.py --count "$1" --surface NOUN:Gender VERB:HebBinyan AUX:HebBinyan DET:PronType PROPN:Gender ExtPos | lt-proc -O generated/hbo-grc/gloss.bin | sed -E 's|(\^[^/]+/[^<]+)<|\1<SOURCE><|g' | cg-conv -a | vislcg3 --dep-delimit -g rempunct.cg3 --out-binary
+}
+
+python3 dix_by_gloss.py "$hin" "$gin" -t NOUN:Gender -s NOUN:Gender -s VERB:HebBinyan -s AUX:HebBinyan -s DET:PronType -t DET:PronType -s PROPN:Gender -t PROPN:Gender generated/hbo-grc/gloss.dix
 
 lt-comp lr generated/hbo-grc/gloss.dix generated/hbo-grc/gloss.bin
 
-cat generated/hbo-grc/hbo.conllu | python3 conllu2apertium.py --count 100 --surface NOUN:Gender VERB:HebBinyan AUX:HebBinyan DET:PronType PROPN:Gender ExtPos | lt-proc -O generated/hbo-grc/gloss.bin | sed -E 's|(\^[^/]+/[^<]+)<|\1<SOURCE><|g' | cg-conv -aC > generated/hbo-grc/hbo.gloss.cg3.txt
-
-#cat generated/hbo-grc/grc.conllu | python3 conllu2apertium.py NOUN:Gender DET:PronType PROPN:Gender | lt-proc -b generated/hbo-grc/gloss.bin | python3 apertium2cg.py | cg-conv -aC > generated/hbo-grc/grc.gloss.cg3.txt
-#cat generated/hbo-grc/grc.conllu | python3 conllu2apertium.py --count 100 NOUN:Gender DET:PronType PROPN:Gender | sed -E 's|\^([^$]*)\$|\^\1/\1\$|g' | python3 apertium2cg.py | cg-conv -aC > generated/hbo-grc/grc.gloss.cg3.txt
-
-cat generated/hbo-grc/grc.conllu | python3 conllu2apertium.py --count 100 --surface NOUN:Gender DET:PronType PROPN:Gender | cg-conv -a | cg-conv -Z --dep-delimit > generated/hbo-grc/grc.gold.bin
-
 cat <<EOF > rempunct.cg3
 DELIMITERS = "<\$\$\$>" ;
-REMCOHORT (PUNCT) ;
-MERGECOHORTS ("<\$1\$5>"v "\$2\$6"v SOURCE VSTR:\$3 VSTR:\$4 "@null" X VSTR:\$4) ("<\\(.+\\)>"r "\\(.+\\)"r SOURCE /^ExtPos=\\(.+\\)\$/r /^\\(@.+\\)\$/r) WITH (c ("<\\(.+\\)>"r "\\(.+\\)"r SOURCE @fixed)) ;
+REMCOHORT (SOURCE PUNCT) ;
+MERGECOHORTS ("<\$1\$5>"v "\$2\$6"v SOURCE VSTR:\$3 VSTR:\$4 "@null" X VSTR:\$4) ("<\\(.+\\)>"r "\\([^<>]+\\)"r SOURCE /^ExtPos=\\(.+\\)\$/r /^\\(@.+\\)\$/r) WITH (c ("<\\(.+\\)>"r "\\([^<>]+\\)"r SOURCE @fixed)) ;
 EOF
 
-cat generated/hbo-grc/hbo.gloss.cg3.txt | vislcg3 --dep-delimit -g rempunct.cg3 --out-binary > generated/hbo-grc/hbo.input.bin
-
-#cat generated/hbo-grc/grc.gloss.cg3.txt | cg-conv -cZ --dep-delimit > generated/hbo-grc/grc.gold.bin
+cat "$hin" | make_hbo 727 > generated/hbo-grc/hbo.input.bin
+cat "$gin" | make_grc 727 > generated/hbo-grc/grc.gold.bin
+cat ~/hbo-UD/UD_Ancient_Hebrew-PTNK/hbo_ptnk-ud-test.conllu | make_hbo 410 > generated/hbo-grc/hbo.test.bin
+cat ~/UD_Ancient_Greek-PTNK/grc_ptnk-ud-test.conllu | make_grc 410 > generated/hbo-grc/grc.test.bin
