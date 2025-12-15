21a09d5825ea8643614f9a676dc55d0bc032f89f
Daniel Swanson <awesomeevildudes@gmail.com>
2025-12-15 13:06:58 -0500

    don't try to run all possible experiments at once

--- hbo-grc.sh
+++ hbo-grc.sh
@@ -4,11 +4,11 @@ hin=~/hbo-UD/UD_Ancient_Hebrew-PTNK/hbo_ptnk-ud-train.conllu
 gin=~/UD_Ancient_Greek-PTNK/grc_ptnk-ud-train.conllu
 
 make_grc() {
-  python3 conllu2apertium.py --count "$1" --surface NOUN:Gender DET:PronType PROPN:Gender | cg-conv -a -Z --dep-delimit
+  python3 conllu2apertium.py --surface NOUN:Gender DET:PronType PROPN:Gender | cg-conv -a -Z --dep-delimit
 }
 
 make_hbo() {
-  python3 conllu2apertium.py --count "$1" --surface NOUN:Gender VERB:HebBinyan AUX:HebBinyan DET:PronType PROPN:Gender ExtPos | lt-proc -O generated/hbo-grc/gloss.bin | sed -E 's|(\^[^/]+/[^<]+)<|\1<SOURCE><|g' | cg-conv -a | vislcg3 --dep-delimit -g rempunct.cg3 --out-binary
+  python3 conllu2apertium.py --surface NOUN:Gender VERB:HebBinyan AUX:HebBinyan DET:PronType PROPN:Gender ExtPos | lt-proc -O generated/hbo-grc/gloss.bin | sed -E 's|(\^[^/]+/[^<]+)<|\1<SOURCE><|g' | cg-conv -a | vislcg3 --dep-delimit -g rempunct.cg3 --out-binary
 }
 
 python3 dix_by_gloss.py "$hin" "$gin" -t NOUN:Gender -s NOUN:Gender -s VERB:HebBinyan -s AUX:HebBinyan -s DET:PronType -t DET:PronType -s PROPN:Gender -t PROPN:Gender generated/hbo-grc/gloss.dix
@@ -21,7 +21,9 @@ REMCOHORT (SOURCE PUNCT) ;
 MERGECOHORTS ("<\$1\$5>"v "\$2\$6"v SOURCE VSTR:\$3 VSTR:\$4 "@null" X VSTR:\$4) ("<\\(.+\\)>"r "\\([^<>]+\\)"r SOURCE /^ExtPos=\\(.+\\)\$/r /^\\(@.+\\)\$/r) WITH (c ("<\\(.+\\)>"r "\\([^<>]+\\)"r SOURCE @fixed)) ;
 EOF
 
-cat "$hin" | make_hbo 727 > generated/hbo-grc/hbo.input.bin
-cat "$gin" | make_grc 727 > generated/hbo-grc/grc.gold.bin
-cat ~/hbo-UD/UD_Ancient_Hebrew-PTNK/hbo_ptnk-ud-test.conllu | make_hbo 410 > generated/hbo-grc/hbo.test.bin
-cat ~/UD_Ancient_Greek-PTNK/grc_ptnk-ud-test.conllu | make_grc 410 > generated/hbo-grc/grc.test.bin
+cat sources/hbo-short-train.conllu | make_hbo > generated/hbo-grc/hbo.input.bin
+cat "$gin" | make_grc > generated/hbo-grc/grc.gold.bin
+cat sources/hbo-short-test.conllu | make_hbo > generated/hbo-grc/hbo.test.bin
+cat sources/hbo-short-dev.conllu | make_hbo > generated/hbo-grc/hbo.dev.bin
+cat ~/UD_Ancient_Greek-PTNK/grc_ptnk-ud-test.conllu | make_grc > generated/hbo-grc/grc.test.bin
+cat ~/UD_Ancient_Greek-PTNK/grc_ptnk-ud-dev.conllu | make_grc > generated/hbo-grc/grc.dev.bin
---
+++ round11_initial_flat_runner.py
@@ -0,0 +1,33 @@
+import concurrent.futures
+import subprocess
+import time
+
+def run_config(grammar, filters, similarity, weights):
+    print('starting', grammar)
+    start = time.time()
+    subprocess.run(['python3', 'round11.py',
+                    'hbo.train.bin', 'grc.train.bin',
+                    '300', grammar,
+                    '--weights', weights,
+                    '--count', filters,
+                    '--beam', filters,
+                    '--rule_count', filters,
+                    '--context_similarity', similarity],
+                   capture_output=True)
+    print('finished', grammar, 'after', time.time() - start, 'seconds')
+
+weight_settings = [('plain', '{}'),
+                   ('feats', '{"missing_feats": 20}')]
+params = []
+for filters in ['10', '25', '50']:
+    for similarity in ['0.7', '0,8', '0.9', '1.0']:
+        for name, weights in 
+            params.append([f'grammars/g_{filters}_{similarity}_{name}.cg3',
+                           filters, similarity, weights])
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
+    for p in params:
+        futures.append(executor.submit(run_config, *p))
+    for future in concurrent.futures.as_completed(futures):
+        pass
+
